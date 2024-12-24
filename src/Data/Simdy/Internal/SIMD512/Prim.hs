1e8203cfd7bd1ca89ab2939e228b8b5f5bf64ea4
ARATA Mizuki <minorinoki@gmail.com>
2024-12-24 22:22:54 +0900

    Workaround https://gitlab.haskell.org/ghc/ghc/-/issues/25606

--- src/Data/Simdy/Internal/SIMD512/Prim.hs
+++ src/Data/Simdy/Internal/SIMD512/Prim.hs
@@ -5,7 +5,7 @@ module Data.Simdy.Internal.SIMD512.Prim
   ( module M
   , module Data.Simdy.Internal.SIMD512.Prim
   ) where
-#if __GLASGOW_HASKELL__ == 912 && !MIN_VERSION_GLASGOW_HASKELL(9, 12, 2, 0) && defined(__GLASGOW_HASKELL_LLVM__)
+#if __GLASGOW_HASKELL__ == 912 && !MIN_VERSION_GLASGOW_HASKELL(9, 12, 2, 0) && defined(USE_LLVM_BACKEND)
 -- GHC 9.12.1 has a bug with broadcast: https://gitlab.haskell.org/ghc/ghc/-/issues/25561
 
 import           GHC.Exts as M hiding (broadcastFloatX16#, broadcastDoubleX8#, broadcastInt8X64#, broadcastInt16X32#, broadcastInt32X16#, broadcastInt64X8#, broadcastWord8X64#, broadcastWord16X32#, broadcastWord32X16#, broadcastWord64X8#)
